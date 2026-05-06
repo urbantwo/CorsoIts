6dc01ba40d2b2283d706cb18b3d8ab5624c692c2
urbantwo <stefano.trevisi.95@gmail.com>
2026-05-06 23:07:35 +0200

    esercizio 1

--- src/esercizi/base/CalcoloInteressi.java
+++ src/esercizi/base/CalcoloInteressi.java
@@ -4,6 +4,36 @@ public class CalcoloInteressi {
 	
 	public static void main(String[] args) {
 		
+		//Dichiariamo e istanziamo le variabili
+		
+		int annoInizio = 2026;
+		double percInteressi = 1.025;
+		
+		double importoInput = 1000.00;
+	
+		double importoCalc = importoInput;
+		
+		//cicliamo per 5 anni per ottenere gli interessi calcolati
+		
+		for (int i = 0; i < 5; i++) {
+			
+			/*
+			 * Calcolo interesse
+			 * 
+			 * avremmo potuto fare anche 
+			 * importoCalc = importoCalc * percInteressi;
+			 */
+			
+			importoCalc *= percInteressi;
+			
+			System.out.println("A fine anno " + annoInizio + " abbiamo " + importoCalc +  "€");
+			
+			//prima che riprenda il ciclo for, aumentiamo di 1 anno l'anno inizio
+			
+			annoInizio ++;
+			
+		}
+		
 	}
 
 }
